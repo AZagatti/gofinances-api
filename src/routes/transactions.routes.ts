071eed7ffeba0957a0da56791eae7e9473598405
azagatti <andre.zagatti1@gmail.com>
2020-04-15 21:56:21 -0300

    feat: added create transaction route

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,25 +1,36 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {
+transactionsRouter.get('/', async (req, res) => {
   // TODO
 });
 
-transactionsRouter.post('/', async (request, response) => {
+transactionsRouter.post('/', async (req, res) => {
+  const { title, value, type, category } = req.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return res.json(transaction);
+});
+
+transactionsRouter.delete('/:id', async (req, res) => {
   // TODO
 });
 
-transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
-});
-
-transactionsRouter.post('/import', async (request, response) => {
+transactionsRouter.post('/import', async (req, res) => {
   // TODO
 });
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+    const categoryRepository = getRepository(Category);
+
+    const checkCategoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    let newCategory: Category;
+
+    if (!checkCategoryExists) {
+      newCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(newCategory);
+    } else {
+      newCategory = checkCategoryExists;
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: newCategory.id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
